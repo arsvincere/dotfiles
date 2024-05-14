466f779db265c0bd59dc2b57f6f3a539705fb820
arsvincere <mr.alexavin@gmail.com>
2024-05-14 11:52:57 +0300

    upd

--- upd.sh
+++ upd.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
 yay -Syu --noconfirm
-pacman -Scc --noconfirm 
-pacman -Rsn --noconfirm $(pacman -Qdtq) 
+
+# Чтобы удалить все кэшированные версии удалённых пакетов и неиспользуемые базы данных, выполните:
+# pacman -Sc --noconfirm 
+
+# Чтобы удалить вообще все файлы из кэша, укажите флаг очистки дважды:
+# pacman -Scc --noconfirm 
+
+# Удалить пакеты, которые больше не требуются как зависимости ("пакеты-сироты"):
+# pacman -Rsn --noconfirm $(pacman -Qdtq) 
+
 # sudo rm -rf ~/.cache/thumbnails/*
 
