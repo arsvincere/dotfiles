108ea6d9a5667d8d9d59b79e4d402e3ede4a3ee7
arsvincere <mr.alexavin@gmail.com>
2024-06-11 19:47:30 +0300

    upd

--- config/nvim/lua/plugin/vimtex.lua
+++ config/nvim/lua/plugin/vimtex.lua
@@ -8,6 +8,7 @@ require('texvim').setup()
 
 vim.g.tex_flavor='latex'
 vim.g.vimtex_view_method='zathura'
+vim.g.vimtex_view_general_viewer='zathura'
 vim.g.vimtex_quickfix_mode=0
 vim.g.nceallevel=1
 vim.g.tex_conceal='abdmg'
