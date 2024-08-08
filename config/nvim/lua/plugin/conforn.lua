e463ffb9c42ecb1518e50b8607ac4c0128eb0039
Alex Avin <mr.alexavin@gmail.com>
2024-08-08 07:46:25 +0300

    update

--- config/nvim/lua/plugin/conforn.lua
+++ config/nvim/lua/plugin/conforn.lua
@@ -13,6 +13,11 @@ require("conform").setup({
     -- Conform will run the first available formatter
     -- javascript = { "prettierd", "prettier", stop_after_first = true },
   },
+  format_on_save = {
+    -- These options will be passed to conform.format()
+    timeout_ms = 2000,
+    lsp_format = "fallback",
+  },
 })
 
 vim.api.nvim_create_autocmd("BufWritePre", {
