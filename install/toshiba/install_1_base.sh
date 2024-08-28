00ffe53d9043905154fa9321d6c54ecb3fcbc95c
Alex Avin <mr.alexavin@gmail.com>
2024-08-28 15:54:58 +0300

    update

---
+++ install/toshiba/install_1_base.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Установка раскладки клавиатуры
+loadkeys ru
+
+# Изменим консольный шрифт на тот, который поддерживает кириллицу
+setfont cyr-sun16
+
+# Подключаем интернет
+wpa_passphrase Keenetic-8471 Q2Wobiko > /etc/wpa_supplicant.conf
+wpa_supplicant -B -Dwext -i wlan0 -c /etc/wpa_supplicant.conf
+dhcpcd wlan0
+
+timedatectl set-ntp true
+
+# Разметка диска
+# fdisk -l
+# fdisk /dev/sda
+
+# Форматирование дисков
+mkswap      /dev/sda1 -L swap
+mkfs.ext4   /dev/sda2 -L root
+
+# Монтирование дисков 
+mount  /dev/sda2 /mnt
+swapon /dev/sda1
+
+# Установка основных пакетов
+pacman -Syy
+pacman -Sy archlinux-keyring
+pacstrap /mnt base base-devel linux linux-firmware dhcpcd neovim networkmanager
+
+# Генерируем fstab
+genfstab -pU /mnt >> /mnt/etc/fstab
+
+echo "-----------------------------------------------------------------------"
+echo ">>>---> Continue:"
+echo "- unmount flash"
+echo "- arch-chroot /mnt"
+echo "- mount flash"
+echo "- run scritp part 2 - chroot"
+
---
+++ install/toshiba/install_3_user.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Автозапуск NetworkManager
+systemctl start NetworkManager.service
+systemctl enable NetworkManager.service
+
+# Подключаем wifi
+nmcli device wifi list
+nmcli device wifi connect Keenetic-8471 password Q2Wobiko
+
+# Добавляем пользователя
+echo "add user 'alex'"
+useradd -m -g users -G wheel -s /bin/bash alex
+
+# Устанавливаем пароль пользователя
+echo "Enter passwd for alex:"
+passwd alex
+
+## Allow members of group wheel to execute any command
+sudo echo "%wheel ALL=(ALL:ALL) ALL" >> /etc/sudoers
+
+# Same thing without a password
+sudo echo "%wheel ALL=(ALL:ALL) NOPASSWD: /usr/bin/pacman" >> /etc/sudoers
+
+echo "Continue:"
+echo " - reboot"
+echo " - login as alex"
+echo " - run install 4 - packages"
