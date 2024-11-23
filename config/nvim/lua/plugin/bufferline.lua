29e98044fbc558ee7aa55b4b0fbcf4dbc6c2fa0b
Alex Avin <mr.alexavin@gmail.com>
2024-11-23 09:49:05 +0300

    upd

--- config/nvim/lua/plugin/bufferline.lua
+++ config/nvim/lua/plugin/bufferline.lua
@@ -5,30 +5,29 @@
 vim.opt.termguicolors = true
 
 require("bufferline").setup {
-    options = {
-        offsets = {
-            {
-                filetype = "NvimTree",
-                text = "󱏒  Project files:",
-                highlight = "Directory",
-                separator = true,
-                text_align = "left",
-            }
-        },
-        color_icons = true,
-        sort_by = 'extension',
-        buffer_close_icon = '󰅖',
-        close_icon = '', -- hide tabs close icon
-        custom_areas = {
-            right = function()
-                local result = {}
+  options = {
+    offsets = {
+      {
+        filetype = "NvimTree",
+        text = "󱏒  File Explorer:",
+        highlight = "Directory",
+        separator = true,
+        text_align = "left",
+      }
+    },
+    color_icons = true,
+    sort_by = 'extension',
+    buffer_close_icon = '󰅖',
+    close_icon = '', -- hide tabs close icon
+    custom_areas = {
+      right = function()
+        local result = {}
+        -- table.insert(result, { text = "󰣇 Arch Linux ", link = "Text" })
+        -- table.insert(result, { text = "NeoVim the best! ", link = "Text" })
+        -- table.insert(result, { text = " NeoVim ", link = "Text" })
 
-                -- table.insert(result, { text = "󰣇 Arch Linux ", link = "Text" })
-                -- table.insert(result, { text = "NeoVim the best! ", link = "Text" })
-                -- table.insert(result, { text = " NeoVim ", link = "Text" })
-
-                return result
-            end,
-        }
+        return result
+      end,
     }
+  }
 }
