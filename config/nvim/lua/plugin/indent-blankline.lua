fd427fac94366a0a2a8ce27699b59592454a5a32
Alex Avin <mr.alexavin@gmail.com>
2024-11-28 21:53:35 +0300

    upd

--- config/nvim/lua/plugin/indent-blankline.lua
+++ config/nvim/lua/plugin/indent-blankline.lua
@@ -2,14 +2,14 @@
 -- Подсветка блоков
 ------------------------------------------------------------------------------
 
-local cmd = vim.cmd             -- execute Vim commands
+local cmd = vim.cmd -- execute Vim commands
 local autocmd = vim.api.nvim_create_autocmd
 
 require("ibl").setup({
-    scope = { enabled = false },
-    })
+  scope = { enabled = false },
+})
 
 cmd [[autocmd FileType dashboard IBLDisable]]
-autocmd('BufRead',    { pattern = '*.py',    command = 'IBLEnable' })
-autocmd('BufRead',    { pattern = '*.pyw',   command = 'IBLEnable' })
-
+cmd [[autocmd FileType txt IBLDisable]]
+autocmd('BufRead', { pattern = '*.py', command = 'IBLEnable' })
+autocmd('BufRead', { pattern = '*.pyw', command = 'IBLEnable' })
